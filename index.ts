5688be608a9410596dea775cda6d3922cb090557
Anton Moskovskii <ant1mas.dev@gmail.com>
2024-02-23 18:19:28 +0500

    Add polyfill for structuredClone

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
+import structuredClone from '@ungap/structured-clone'
+
 import methodConvert from '@/methods/convert'
+
+if (!('structuredClone' in globalThis)) {
+  // @ts-ignore
+  globalThis.structuredClone = structuredClone
+}
 
 const numberToWordsRu = {
   convert: methodConvert,
